a1b623c641998a88e0c48fdaf7d04668d4790ff7
lnedobelskyi <liubomyrned@gmail.com>
2018-08-01 15:40:42 +0300

    HomeWork

--- src/main/java/BadCodeExample.java
+++ src/main/java/BadCodeExample.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Sleeper;
 
 import java.util.List;
 
@@ -15,17 +16,28 @@ public class BadCodeExample {
         WebElement queryField = browser.findElement(By.name("q"));
         queryField.sendKeys("selenium");
         queryField.sendKeys (Keys.ENTER);
+        Thread.sleep(3000);
 
         // Verify that results list contains 10 elements
-        List<WebElement> searchResults = browser.findElements(By.xpath("//div[@class='srg']/div[@class='g']"));
+        List<WebElement> searchResults = browser.findElements(By.cssSelector("div.srg div.g"));
         System.out.println("Results count: "+searchResults.size());
 
         // Verify that each result item contains searchterm
         for (WebElement searchResult: searchResults) {
            String searchResultText = searchResult.getText();
            System.out.println(searchResultText);
+           if (searchResults.size()<10) {
+               System.out.println("Results count "+ searchResults.size()+" is incorrect");
+           }else{
+               System.out.println("Results count "+ searchResults.size()+" is correct");
+           }
         }
 
+
+        //Home Task #2:
+        //Use if/else operators to add simple verifications in Google search scenario
+        //- Print message "Results count is correct" if there are 10 results and print "Results count is incorrect" if not 10.
+        //- Print message "Searchterm found" if searchterm is present in result item and "Searchterm not found" if not.
         //browser.close();
     }
 }
