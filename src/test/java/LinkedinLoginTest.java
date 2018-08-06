ea25a1da2ef2ca9c70164956378ebd54999825e4
lnedobelskyi <liubomyrned@gmail.com>
2018-08-06 14:51:21 +0300

    Lesson 3

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -9,7 +9,23 @@ public class LinkedinLoginTest {
 
 
     @Test
-    public void successfullLoginTest (){
+    public void successfullLoginTest () throws InterruptedException {
+        System.setProperty("webdriver.gecko.driver","/Users/liubomyrned/Downloads/geckodriver");
+        WebDriver browser = new FirefoxDriver();
+        browser.get ("https://www.linkedin.com/");
+        WebElement userEmailField = browser.findElement(By.xpath("//*[@id=\"login-email\"]"));
+        WebElement userPasswordField = browser.findElement(By.xpath("//*[@id=\"login-password\"]"));
+        WebElement signInButton = browser.findElement(By.xpath("//*[@id=\"login-submit\"]"));
+        userEmailField.sendKeys("liubomyrned21@gmail.com");
+        userPasswordField.sendKeys("VCM-TuY-EVZ-r4p");
+        signInButton.click();
+
+        Thread.sleep(3000);
+
+        WebElement Titleafterlogin = browser.findElement(By.xpath("//*[@id=\"ember4608\"]"));
+        Assert.assertEquals(Titleafterlogin.getText(), "Добро пожаловать, lned!");
+        WebElement URLafterlogin = browser.findElement(By.xpath("//*[@id=\"ember4608\"]"));
+        Thread.sleep(3000);
 
     }
     @Test
@@ -29,6 +45,8 @@ public class LinkedinLoginTest {
         WebElement alertBox = browser.findElement(By.xpath("//*[@role='alert']"));
         Assert.assertEquals(alertBox.getText(), "При заполнении формы были допущены ошибки. Проверьте и исправьте отмеченные поля.", "Alert box has incorrect message");
         Thread.sleep(3000);
+
+
         browser.close();
     }
 }
