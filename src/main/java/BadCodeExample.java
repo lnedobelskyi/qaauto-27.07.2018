8b2dd144cc30a5de67760c857afefbbc8d89d073
lnedobelskyi <liubomyrned@gmail.com>
2018-08-07 22:42:31 +0300

    Lesson 3 (homework)

--- src/main/java/BadCodeExample.java
+++ src/main/java/BadCodeExample.java
@@ -25,21 +25,13 @@ public class BadCodeExample {
 
         // Verify that each result item contains searchterm
         for (WebElement searchResult: searchResults) {
-           String searchResultText = searchResult.getText();
-           if (searchResultText.contains("Selenium")) {
-               System.out.println("Searchterm found");
-           }else{
-               System.out.println("Searchterm is not found");
-           }
-
-           }
+            String searchResultText = searchResult.getText();
+            if (searchResultText.contains("Selenium")) {
+                System.out.println("Searchterm found");
+            } else {
+                System.out.println("Searchterm is not found");
+            }
         }
-
-
-        //Home Task #2:
-        //Use if/else operators to add simple verifications in Google search scenario
-        //- Print message "Results count is correct" if there are 10 results and print "Results count is incorrect" if not 10.
-        //- Print message "Searchterm found" if searchterm is present in result item and "Searchterm not found" if not.
-        //browser.close();
+        }
     }
 }
--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -3,45 +3,47 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LinkedinLoginTest {
+    WebDriver browser;
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.setProperty("webdriver.gecko.driver","/Users/liubomyrned/Downloads/geckodriver");
+        browser = new FirefoxDriver();
+        browser.get ("https://www.linkedin.com/");
+    }
+    @AfterMethod
+    public void afterMethod(){
+        browser.close();
+    }
 
 
     @Test
     public void successfullLoginTest () throws InterruptedException {
-        System.setProperty("webdriver.gecko.driver","/Users/liubomyrned/Downloads/geckodriver");
-        WebDriver browser = new FirefoxDriver();
-        browser.get ("https://www.linkedin.com/");
-        WebElement userEmailField = browser.findElement(By.xpath("//*[@id=\"login-email\"]"));
-        WebElement userPasswordField = browser.findElement(By.xpath("//*[@id=\"login-password\"]"));
-        WebElement signInButton = browser.findElement(By.xpath("//*[@id=\"login-submit\"]"));
-        userEmailField.sendKeys("liubomyrned21@gmail.com");
-        userPasswordField.sendKeys("VCM-TuY-EVZ-r4p");
-        signInButton.click();
+
+        LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage(browser);
+        linkedinLoginPage.login("liubomyrned21@gmail.com", "VCM-TuY-EVZ-r4p");
 
         Thread.sleep(3000);
 
-        WebElement Titleafterlogin = browser.findElement(By.xpath("//*[@id=\"ember4608\"]"));
-        Assert.assertEquals(Titleafterlogin.getText(), "Добро пожаловать, lned!");
-        WebElement specificWebElement = browser.findElement(By.xpath("//*[@id=\"feed-tab-icon\"]"));
-        
+        String pageTitle = browser.getTitle();
+        String pageUrl = browser.getCurrentUrl();
+        WebElement profileNavigationItem = browser.findElement(By.xpath("//*[@id='profile-nav-item']"));
+        Assert.assertEquals(pageTitle, "LinkedIn", "Home page Title is wrong");
+
+        Assert.assertEquals(pageUrl, "https://www.linkedin.com/feed/", "Home page URL is wrong");
+
+        Assert.assertTrue(profileNavigationItem.isDisplayed(), "'profileNavigationItem' is not displayed on Home page.");
+
         Thread.sleep(3000);
 
-        //Home Task #3:
-        //Create successfulLoginTest()
-        //- Register separate test account on Linkedin
-        //- Use negativeLoginTest() as an example
-        //- In your positive test you need to have 3 assertions:
-        //Assert page URL after login;
-        //Assert page Title after login;
-        //Assert some specific WebElement is displayed after login
     }
     @Test
     public void negativeLoginTest () throws InterruptedException {
-        System.setProperty("webdriver.gecko.driver","/Users/liubomyrned/Downloads/geckodriver");
-        WebDriver browser = new FirefoxDriver();
-        browser.get ("https://www.linkedin.com/");
         WebElement userEmailField = browser.findElement(By.xpath("//*[@id=\"login-email\"]"));
         WebElement userPasswordField = browser.findElement(By.xpath("//*[@id=\"login-password\"]"));
         WebElement signInButton = browser.findElement(By.xpath("//*[@id=\"login-submit\"]"));
@@ -56,6 +58,5 @@ public class LinkedinLoginTest {
         Thread.sleep(3000);
 
 
-        browser.close();
     }
 }
