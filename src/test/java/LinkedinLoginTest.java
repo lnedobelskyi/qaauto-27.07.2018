62a4e687ca039d14a61cb6e5424bf29c2a4b47e0
lnedobelskyi <liubomyrned@gmail.com>
2018-08-10 19:20:27 +0300

    Lesson4HomeTask

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -26,20 +26,14 @@ public class LinkedinLoginTest {
     public void successfullLoginTest () throws InterruptedException {
         LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage(browser);
         linkedinLoginPage.login("liubomyrned21@gmail.com", "VCM-TuY-EVZ-r4p");
-
         Thread.sleep(3000);
-
         String pageTitle = browser.getTitle();
         String pageUrl = browser.getCurrentUrl();
         //WebElement profileNavigationItem = browser.findElement(By.xpath("//*[@id='profile-nav-item']"));
-
         Thread.sleep(5000);
         Assert.assertEquals(pageTitle, "LinkedIn", "Home page Title is wrong");
-
         Assert.assertEquals(pageUrl, "https://www.linkedin.com/feed/", "Home page URL is wrong");
-
         Assert.assertTrue(linkedinLoginPage.isProfileNavigationItemPresent(), "'profileNavigationItem' is not displayed on Home page.");
-
         Thread.sleep(3000);
         }
 
@@ -47,10 +41,7 @@ public class LinkedinLoginTest {
     public void negativeLoginTest () throws InterruptedException {
         LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage(browser);
         linkedinLoginPage.login("tph", "tph2");
-
-
         Thread.sleep(3000);
-
         WebElement alertBox = browser.findElement(By.xpath("//*[@role='alert']"));
         Assert.assertEquals(alertBox.getText(), "При заполнении формы были допущены ошибки. Проверьте и исправьте отмеченные поля.", "Alert box has incorrect message");
         Thread.sleep(3000);
